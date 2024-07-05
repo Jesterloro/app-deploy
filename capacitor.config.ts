91bcee5f388059b48c2c33806b90920aaa6ddea4
Jesterloro <lorojester@gmail.com>
2024-07-05 14:54:41 +0800

    first-commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.frontend.app',
+  appName: 'frontend',
+  webDir: 'dist',
+  bundledWebRuntime:false
+};
+
+export default config;
